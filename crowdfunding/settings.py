c21285c3c01b0bbc671134ef24855763958240ed
pollct <poll.cedeno@outlook.com>
2024-05-15 16:07:29 -0500

    Update settings.py

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -159,3 +159,17 @@ else:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',  # Nivel de registro que se desea ver
+    },
+}
