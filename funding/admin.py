99b1bbe7d700cecc81cedfce9b9daafae3457e08
pollct <poll.cedeno@outlook.com>
2024-05-15 15:34:49 -0500

    Initial commit

---
+++ funding/admin.py
@@ -0,0 +1,68 @@
+from django.contrib import admin
+
+# Register your models here.
+from funding.models import Categoria, Campania, FormaPago, Beneficiario, FormaPagoUsuario, HistorialPago
+
+admin.site.site_header = 'Administrador de BeeFunding'
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion', 'fecha_registro', 'fecha_ult_act')
+    
+@admin.register(Campania)
+class CampaniaAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'nombre', 
+        'descripcion',
+        'foto', 
+        'monto_x_recaudar', 
+        'monto_recaudado',
+        'fecha_inicio',
+        'fecha_cierre',
+        'fecha_registro',
+        'fecha_ult_act'
+    )
+    
+@admin.register(FormaPago)
+class FormaPagoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+        'imagen',
+        'fecha_registro',
+        'fecha_ult_act'
+    )
+    
+@admin.register(Beneficiario)
+class Beneficiario(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre', 
+        'fecha_nacimiento',
+        'pais',
+        'ciudad',
+        'email',
+        'telefono',
+        'fecha_registro',
+        'fecha_ult_act' 
+    )
+    
+@admin.register(FormaPagoUsuario)
+class FormaPagoUsuarioAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'usuario',
+        'forma_pago',
+        'fecha_registro',
+        'fecha_ult_act'
+    )
+    
+@admin.register(HistorialPago)
+class HistorialPagoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'usuario',
+        'fecha_registro',
+        'fecha_ult_act'
+    )
---
+++ funding/models/forma_pago.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class FormaPago(models.Model):
+    nombre = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='imagenes/', null=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True, null=True)
+    fecha_ult_act = models.DateTimeField(auto_now=True, null=True)
+    estado = models.BooleanField(null=True)
+
+    def __str__(self) -> str:
+        return f'{self.nombre} ({self.id})'
+
+    class Meta:
+        db_table = 'st_formas_pago'
+        verbose_name = 'Forma de Pago'
+        verbose_name_plural = 'Formas de Pago'
